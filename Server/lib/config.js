e0b7bfcd5da58022116e916ef5a4d9ad0fe39ec5
ruitang2create <trtdytr93629@gmail.com>
2021-07-05 06:13:27 -0500

    Updated gitignore

--- Server/lib/config.js
+++
@@ -1,9 +0,0 @@
-// const serverAddress = 'http://18.191.236.237';
-const serverAddress = 'http://192.168.100.108';
-const serverPort = '8080';
-const serverURL = {
-    address: serverAddress,
-    port: serverPort
-};
-
-module.exports = serverURL;
